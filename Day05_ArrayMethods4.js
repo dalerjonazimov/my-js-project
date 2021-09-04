518d8ed76ccbcb7c5c10fc5eff7daf1a0200b95e
techproed <abayram.techproed@gmail.com>
2021-09-04 15:15:44 -0500

    day05

--- Day05_ArrayMethods4.js
+++ Day05_ArrayMethods4.js
@@ -1,35 +1,35 @@
 /*
-ARRAYS:
-Arrays are special type of objects
-We use index to get teh values in array
-We use [] to create arrays
+  ARRAYS:
 */
-
 console.clear();
 
-//8. concat -> combines arrays
-var drinks=['water','coke','fanta','tea']
-var snacks=['sunchips','gold fish','nuts','popcorn']
-const favFoods=drinks.concat(snacks);
-console.log(favFoods);//["water", "coke", "fanta", "tea", "sunchips", "gold fish", "nuts", "popcorn"]
+// 8. concat()
+var drinks = ['water', 'coke', 'fanta','tea']
+var snacks = ['sunchips', 'gold fish', 'nuts', 'popcorn']
+const favFoods =drinks.concat(snacks);
+console.log(favFoods); // ["water", "coke", "fanta", "tea", "sunchips", "gold fish", "nuts", "popcorn"]
 
+// 9. slice(startIndex(included), endIndex(excluded)) 
+// -> returns the piece in between the start and end index
+// cut the slice (similar to substring in String)
+const myFoods= favFoods.slice(1,5);
+console.log(myFoods); // ["coke", "fanta", "tea", "sunchips"]
 
-//9. slice(startIndex(included),endIndex(excluded)) -> returns the piece in between the start and end index(similar to substring)
-const myFoods = favFoods.slice(1,5);
-console.log(myFoods);//["coke", "fanta", "tea", "sunchips"]
+const favSnacks = snacks.slice(2); // returns from index 2 till endIndex
+console.log(favSnacks); // ["nuts", "popcorn"]
 
-//
-const favSnacks=snacks.slice(2);//returns from index2 till endIndex
-console.log(favSnacks);//["nuts", "popcorn"]
+// negative index means start from the end
+const sliceDrinks = drinks.slice(-1); // start from last index till the end
+console.log(sliceDrinks); // ["tea"]
 
-//negatiuve index means start from the end
-const slicedDrinks=drinks.slice(-1); //start from last index till the end
-console.log(slicedDrinks);//["tea"]
+//10. join -> returns a string. we can combine the items of the array using special character
+//water - coke - fanta - tea
+const joinedItems=drinks.join(' - ')
+console.log(joinedItems);//"water - coke - fanta - tea"
 
+const joinedItems1=drinks.join('')
+console.log(joinedItems1);//"watercokefantatea"
 
-
-
-
-
-
+const joinedItems2=drinks.join(',')
+console.log(joinedItems2);//"water,coke,fanta,tea"
 
